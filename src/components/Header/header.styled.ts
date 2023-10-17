360bbf26d48d7b2100cb94f55f66e2b5019e623a
geovanne <geovamelo4431@gmail.com>
2023-10-16 22:15:54 -0300

    feat: Iniciado o Front-End

---
+++ src/components/Header/header.styled.ts
@@ -0,0 +1,8 @@
+"use client";
+import { styled } from "@mui/material";
+
+export const header = styled('header')({
+    padding: "4rem",
+    display: "flex",
+    backgroundColor: "rgb(251, 91, 77)"
+})
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as Styled from './header.styled';
+
+const Header = () => {
+  return (
+    <Styled.header>
+        This is my header
+    </Styled.header>
+  )
+}
+
+export default Header
